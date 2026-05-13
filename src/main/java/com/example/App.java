a9a2f928c5a102146f9ab8cc7b1015c305a1ac36
Shuchithra N K <shuchithrank1234gmail.com>
2026-05-13 23:08:05 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       int a=10;
+       int b=20;
+       int sum=a+b;
+       System.out.println("Sum="+sum);
+    }
+}
